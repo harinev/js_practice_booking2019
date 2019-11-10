99dfe99efd870675b05ffb88acb9cb0955f69cc4
harine <harinev@outlook.com>
2019-11-10 19:55:34 +0000

    partially solved until reverseWords function

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,34 +1,58 @@
 function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  for (n=0; n<= word.length; n++){
+    const upperFirst= word[n][0]
+    return upperFirst.toUpperCase()+word.slice(1);
+  };
 }
 
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  //get firstname[0] and lastname[0] and concatenate
+  const firstInitials=firstName[0];
+  const lastInitials=lastName[0];
+  return (firstInitials+"."+ lastInitials);
 }
 
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  // vatamount is defined, add it to the original price for the addvat price
+  const vatAmount=Number((originalPrice*vatRate/100).toFixed(2));
+  // const vatAddedPrice= originalPrice+vatAmount;
+  return (originalPrice+vatAmount);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  // calculate reduction amount 
+  const reductionAmt= Number(originalPrice*reduction/100).toFixed(2);
+  //calculate net amount for sale after discount
+  return (originalPrice-reductionAmt);
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
-}
+  
+    //get the length of the string and mod 2=0? then evenstr: oddstr
+    if (str.length% 2===1) {
+      const oddStringIndex= (str.length-1)/2;
+      return str[oddStringIndex];
+    } else {
+      const oddStringIndex=(str.length)/2;
+      return str.slice(oddStringIndex-1, oddStringIndex+1)
+    }
+    // if odd string, get middle char index
+    //if even string, get 2 middle char index
+
+  }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  // into array-split the words-reverse-join 
+  return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
